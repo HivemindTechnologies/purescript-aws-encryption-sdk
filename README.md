167a1a266d3560d0d3ac883992de7d028d6d62ec
hivemind-js <jan.schulte@hivemindtechnologies.com>
2021-03-04 08:09:12 +0000

    Prepare v0.14.0

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # purescript-aws-encryption-sdk
+
+Purescript wrapper for the aws-encryption-sdk. 
+
+## Usage
+
+See [test folder](./test/) for how to decrypt & encrypt.
--- test/AWS/Crypto/CryptoSpec.purs
+++ test/AWS/Crypto/CryptoSpec.purs
@@ -15,7 +15,7 @@ spec =
         keyring <-
           liftEffect
             $ makeKeyring
-                (Arn "arn:aws:kms:us-west-2:658956600833:key/b3537ef1-d8dc-4780-9f5a-55776cbb2f7f")
+                (Arn "arn:aws:kms:<<REGION>>:<<ACCOUNT>>:key/<<KEY_ID>>")
                 []
         let
           testData = "some-data"
